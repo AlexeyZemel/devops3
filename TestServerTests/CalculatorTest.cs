729a8b8781f103e9f3f186815f865719f75de1b7
Alexey <114217985+AlexeyZemel@users.noreply.github.com>
2024-10-21 19:34:46 +0400

    Update CalculatorTest.cs

--- TestServerTests/CalculatorTest.cs
+++ TestServerTests/CalculatorTest.cs
@@ -15,7 +15,7 @@ public class CalculatorTest
             new() { Id = 0, Value = 2 }
         };
 
-        var expectedSum = 3;
+        var expectedSum = 100;
 
         var sum = Calculator.Sum(numbers);
 
